e283550c6eb65fdd145591d00b81f818a300e058
Thu Ta <thuta@Thus-MacBook-Air.local>
2025-11-30 14:16:50 +0630

    section & student migrations

---
+++ database/migrations/2025_11_30_070008_create_sections_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('program_level_id')->constrained();
+            $table->foreignId('exam_cycle_id')->constrained();
+            $table->foreignId('section_type_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->string('code')->unique();
+            $table->integer('capacity')->default(0);
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sections');
+    }
+};
---
+++ database/migrations/2025_11_30_072007_create_students_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->string('nrc')->unique();
+            $table->date('dob');
+            $table->enum('gender', ['Male', 'Female']);
+            $table->string('phone');
+            $table->string('email');
+            $table->string('edu_mail')->nullable()->unique();
+            $table->text('address');
+            $table->string('profile');
+            $table->text('education');
+            $table->text('occupation');
+            $table->string('guardian_name');
+            $table->string('relationship');
+            $table->string('guardian_phone');
+            $table->string('guardian_email')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
